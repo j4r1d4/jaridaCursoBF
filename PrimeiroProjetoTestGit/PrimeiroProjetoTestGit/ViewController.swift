277eb623c21a6b230b15d452e6ad2e3a44f47498
Járida Freire <jarida.xd@gmail.com>
2022-10-11 15:14:44 -0300

    Exercícios respondidos do caio

---
+++ PrimeiroProjetoTestGit/PrimeiroProjetoTestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrimeiroProjetoTestGit
+//
+//  Created by Caio Fabrini on 22/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // meu primeiro comentario
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# jaridaCursoBF
+# primeiroProjetoTurmaBF
+t
